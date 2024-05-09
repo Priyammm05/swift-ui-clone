b86301da8659699790dc236c2e0d886c897a2672
Priyammm05 <priyam05soni@gmail.com>
2024-05-09 17:52:39 +0530

    Initial Commit

---
+++ swift-ui-clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-ui-clone
+//
+//  Created by Priyam Soni on 09/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-ui-clone/swift_ui_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_cloneApp.swift
+//  swift-ui-clone
+//
+//  Created by Priyam Soni on 09/05/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
