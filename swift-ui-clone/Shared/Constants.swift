401026004c1db02e3940fe2dc7a12bc4a4bb19cb
Priyammm05 <priyam05soni@gmail.com>
2024-05-10 11:26:49 +0530

    Added the constant file

---
+++ swift-ui-clone/Shared/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  swift-ui-clone
+//
+//  Created by Priyam Soni on 09/05/24.
+//
+
+import Foundation
+
+struct Constants {
+    static let randomImage = "https://images.unsplash.com/photo-1493673272479-a20888bcee10?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=803&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTcxNTI1NzY1Mg&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1512"
+}
