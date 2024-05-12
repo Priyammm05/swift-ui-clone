2c0f7df487bc177c66c3999958b922a62b4a618f
Priyammm05 <priyam05soni@gmail.com>
2024-05-12 10:44:24 +0530

    Colors file for the spotify

---
+++ swift-ui-clone/Spotify/Core/SpotifyPlaylistView.swift
@@ -0,0 +1,18 @@
+//
+//  SpotifyPlaylistView.swift
+//  swift-ui-clone
+//
+//  Created by Priyam Soni on 11/05/24.
+//
+
+import SwiftUI
+
+struct SpotifyPlaylistView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpotifyPlaylistView()
+}
