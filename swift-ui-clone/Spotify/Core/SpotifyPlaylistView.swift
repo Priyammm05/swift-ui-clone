e6c21f7a36afcb6cdc6bfbefd751e7ff6cbdbab3
Priyammm05 <priyam05soni@gmail.com>
2024-05-12 11:40:05 +0530

    Updated the PlayListView

--- swift-ui-clone/Spotify/Core/SpotifyPlaylistView.swift
+++ swift-ui-clone/Spotify/Core/SpotifyPlaylistView.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct SpotifyPlaylistView: View {
+    
+    var product: Product = .mock
+    var user: User = .mock
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color.spotifyBlack.ignoresSafeArea()
+            
+            ScrollView(.vertical){
+                LazyVStack(spacing: 12){
+                    PlaylistHeaderCell(
+                        height: 250,
+                        title: product.title,
+                        subtitle: product.brand,
+                        imageName: product.thumbnail
+                    )
+                    
+                    PlaylistDescriptionCell(
+                        descriptionText: product.description,
+                        userName: user.firstName,
+                        subHeadline: product.category.capitalized,
+                        onAddToPlaylistPressed: nil,
+                        onDownloadPressed: nil,
+                        onSharePressed: nil,
+                        onEllipsisPressed: nil,
+                        onAShufflePressed: nil,
+                        onPlayPressed: nil
+                    )
+                }
+            }
+            .scrollIndicators(.hidden)
+        }
     }
 }
 
