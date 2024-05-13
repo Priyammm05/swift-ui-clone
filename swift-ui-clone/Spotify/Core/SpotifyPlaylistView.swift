fbd2526a9a870998be7831e64528339a78c9dacb
Priyammm05 <priyam05soni@gmail.com>
2024-05-13 12:34:03 +0530

    Added the logic for the header and updated the view with SongRowCell Component

--- swift-ui-clone/Spotify/Core/SpotifyPlaylistView.swift
+++ swift-ui-clone/Spotify/Core/SpotifyPlaylistView.swift
@@ -6,11 +6,17 @@
 //
 
 import SwiftUI
+import SwiftfulRouting
 
 struct SpotifyPlaylistView: View {
     
     var product: Product = .mock
     var user: User = .mock
+    
+    @State private var products: [Product] = []
+    @State private var showHeader: Bool = true
+    
+    @Environment(\.router) var router
     
     var body: some View {
         ZStack{
@@ -24,6 +30,9 @@ struct SpotifyPlaylistView: View {
                         subtitle: product.brand,
                         imageName: product.thumbnail
                     )
+                    .readingFrame { frame in
+                        showHeader = frame.maxY < 150
+                    }
                     
                     PlaylistDescriptionCell(
                         descriptionText: product.description,
@@ -36,13 +45,76 @@ struct SpotifyPlaylistView: View {
                         onAShufflePressed: nil,
                         onPlayPressed: nil
                     )
+                    .padding(.horizontal, 16)
+                    
+                    ForEach(products){ product in
+                        SongRowCell(
+                            imageSize: 50,
+                            imageName: product.firstImage,
+                            title: product.title,
+                            subtitle: product.brand,
+                            onCellPressed: {
+                            },
+                            onEllipsisPressed: {
+                        
+                           }
+                        )
+                    }
+                    .padding(.leading, 16)
                 }
             }
             .scrollIndicators(.hidden)
+            
+            header
+                .frame(maxHeight: .infinity, alignment: .top)
+            
+        }
+        .task {
+            await getData()
+        }
+        .toolbar(.hidden, for: .navigationBar)
+    }
+    
+    
+    private func getData() async {
+        do {
+            products = try await DatabaseHelper().getProducts()
+        } catch {
+            
         }
     }
 }
 
 #Preview {
-    SpotifyPlaylistView()
+    RouterView { _ in
+        SpotifyPlaylistView()
+    }
+}
+
+extension SpotifyPlaylistView{
+    private var header: some View{
+        ZStack{
+            Text(product.title)
+                .font(.headline)
+                .padding(.vertical, 20)
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .background(Color.spotifyBlack)
+                .offset(y: showHeader ? 0 : -40)
+                .opacity(showHeader ? 1 : 0)
+            
+            Image(systemName: "chevron.left")
+                .font(.title3)
+                .padding(10)
+                .background(showHeader ? Color.black.opacity(0.001) : Color.spotifyGray.opacity(0.7))
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                .onTapGesture {
+                    router.dismissScreen()
+                }
+                .padding(.leading, 10)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+        }
+        .foregroundStyle(.spotifyWhite)
+        .animation(.smooth(duration: 0.2), value: showHeader)
+    }
 }
