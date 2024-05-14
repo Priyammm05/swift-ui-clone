0069eea09932f6eb43c6da37dfc4a2c616fdf3d8
Priyammm05 <priyam05soni@gmail.com>
2024-05-14 20:19:04 +0530

    Created the BumbleCardViewComponent

---
+++ swift-ui-clone/Bumble/Component/BumbleCardView.swift
@@ -0,0 +1,18 @@
+//
+//  BumbleCardView.swift
+//  swift-ui-clone
+//
+//  Created by Priyam Soni on 14/05/24.
+//
+
+import SwiftUI
+
+struct BumbleCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BumbleCardView()
+}
