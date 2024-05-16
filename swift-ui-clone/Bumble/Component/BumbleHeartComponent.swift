218e361c6ce1e3a138c14434092871f25eebdf20
Priyammm05 <priyam05soni@gmail.com>
2024-05-16 20:13:45 +0530

    Created the reusable circular heart component

---
+++ swift-ui-clone/Bumble/Component/BumbleHeartComponent.swift
@@ -0,0 +1,31 @@
+//
+//  BumbleHeartComponent.swift
+//  swift-ui-clone
+//
+//  Created by Priyam Soni on 16/05/24.
+//
+
+import SwiftUI
+
+struct BumbleHeartView: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(.bumbleYellow)
+                .frame(width: 40, height: 40)
+            
+            Image(systemName: "bubble.fill")
+                .foregroundStyle(.bumbleBlack)
+                .font(.system(size: 22))
+                .offset(y: 2)
+            
+            Image(systemName: "heart.fill")
+                .foregroundStyle(.bumbleYellow)
+                .font(.system(size: 10))
+        }
+    }
+}
+
+#Preview {
+    BumbleHeartView()
+}
