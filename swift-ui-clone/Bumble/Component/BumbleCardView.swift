a4c9450e718f7b917cb35ef524e2a000edcff10a
Priyammm05 <priyam05soni@gmail.com>
2024-05-17 12:17:07 +0530

    Added the location and Footer section in Card view

--- swift-ui-clone/Bumble/Component/BumbleCardView.swift
+++ swift-ui-clone/Bumble/Component/BumbleCardView.swift
@@ -12,6 +12,12 @@ struct BumbleCardView: View {
     var user: User = .mock
     @State private var cardFrame: CGRect = .zero
     
+    var onSendAComplimentPressed: (() -> Void)? = nil
+    var onSuperLikePressed: (() -> Void)? = nil
+    var onXmarkPressed: (() -> Void)? = nil
+    var onCheckmarkPressed: (() -> Void)? = nil
+    var onHideAndReportPressed: (() -> Void)? = nil
+    
     var body: some View {
         ScrollView(.vertical){
             LazyVStack(spacing: 0){
@@ -25,10 +31,30 @@ struct BumbleCardView: View {
                 myInterestsSection
                     .padding(.horizontal, 24)
                     .padding(.vertical, 24)
+                
+                ForEach(user.images, id: \.self){ image in
+                    ImageLoaderView(urlString: image)
+                        .frame(height: cardFrame.height)
+                }
+                
+                locationSection
+                    .padding(.horizontal, 24)
+                    .padding(.vertical, 24)
+                
+                footerSection
+                    .padding(.horizontal, 32)
+                    .padding(.vertical, 60)
+                
             }
         }
         .scrollIndicators(.hidden)
         .background(.bumbleBackground)
+        .overlay(
+            superLikeButton
+                .padding(24)
+            
+            , alignment: .bottomTrailing
+        )
         .cornerRadius(32)
         .readingFrame{ frame in
             cardFrame = frame
@@ -112,7 +138,7 @@ extension BumbleCardView{
             .background(.bumbleYellow)
             .cornerRadius(32)
             .onTapGesture {
-//                onSendAComplimentPressed?()
+                onSendAComplimentPressed?()
             }
         }
         .frame(maxWidth: .infinity, alignment: .leading)
@@ -132,4 +158,79 @@ extension BumbleCardView{
         }
         .frame(maxWidth: .infinity, alignment: .leading)
     }
+    
+    private var locationSection: some View{
+        VStack(alignment: .leading, spacing: 12) {
+            HStack(spacing: 8){
+                Image(systemName: "mappin.and.ellipse.circle.fill")
+                Text(user.firstName + "'s Location")
+            }
+            .foregroundStyle(.bumbleGray)
+            .font(.body)
+            .fontWeight(.medium)
+            
+            Text("5 Km away")
+                .font(.headline)
+                .foregroundStyle(.bumbleBlack)
+            
+            InterestPillView(iconName: nil, emoji: "🇮🇳", text: "Lives in Whitefield, Bengaluru")
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var footerSection: some View{
+        VStack(spacing: 24){
+            HStack(spacing: 0){
+                Circle()
+                    .fill(.bumbleYellow)
+                    .overlay (
+                        Image(systemName: "xmark")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                    )
+                    .frame(width: 60, height: 60)
+                    .onTapGesture {
+                        onXmarkPressed?()
+                    }
+                
+                Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
+                
+                Circle()
+                    .fill(.bumbleYellow)
+                    .overlay (
+                        Image(systemName: "checkmark")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                    )
+                    .frame(width: 60, height: 60)
+                    .onTapGesture {
+                        onCheckmarkPressed?()
+                    }
+            }
+            
+            Text("Hide and Report")
+                .font(.headline)
+                .foregroundStyle(.bumbleGray)
+                .padding(8)
+                .background(Color.black.opacity(0.001))
+                .onTapGesture {
+                    onHideAndReportPressed?()
+                }
+        }
+    }
+    
+    private var superLikeButton: some View {
+        Image(systemName: "hexagon.fill")
+            .foregroundStyle(.bumbleYellow)
+            .font(.system(size: 60))
+            .overlay(
+                Image(systemName: "star.fill")
+                    .foregroundStyle(.bumbleBlack)
+                    .font(.system(size: 30, weight: .medium))
+            )
+            .onTapGesture {
+                onSuperLikePressed?()
+            }
+    }
+    
 }
