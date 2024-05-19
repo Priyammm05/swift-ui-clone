43f84fa251ea7fd039857a3e930f954b6f988aa7
Priyammm05 <priyam05soni@gmail.com>
2024-05-19 14:07:16 +0530

    Updated the HomeView and Added the movie component

--- swift-ui-clone/Netflix/Core/NetflixHomeView.swift
+++ swift-ui-clone/Netflix/Core/NetflixHomeView.swift
@@ -11,10 +11,32 @@ struct NetflixHomeView: View {
     
     @State private var filters = FilterModel.mockArray
     @State private var selectedFilter: FilterModel? = nil
+    @State private var fullHeaderSize: CGSize = .zero
+    @State private var scrollViewOffset: CGFloat = 0
+    
+    @State private var heroProduct: Product? = nil
+    @State private var currentUser: User? = nil
+    @State private var productRows: [ProductRow] = []
     
     var body: some View {
-        ZStack{
+        ZStack(alignment: .top){
             Color.netflixBlack.ignoresSafeArea()
+            
+            ScrollView(.vertical){
+                VStack(spacing: 8){
+                    Rectangle()
+                        .opacity(0)
+                        .frame(height: fullHeaderSize.height)
+                    
+                    if let heroProduct{
+                        heroCell(product: heroProduct)
+                        .padding(.top, 26)
+                    }
+                    
+                    categoryRows
+                }
+            }
+            .scrollIndicators(.hidden)
             
             VStack(spacing: 0){
                 header
@@ -31,12 +53,37 @@ struct NetflixHomeView: View {
                     }
                 )
                 .padding(.top, 16)
-                
-                Spacer()
+                .readingFrame { frame in
+                    fullHeaderSize = frame.size
+                }
                 
             }
         }
         .foregroundStyle(.netflixWhite)
+        .task {
+            await getData()
+        }
+        .toolbar(.hidden, for: .navigationBar)
+    }
+    
+    private func getData() async {
+        guard productRows.isEmpty else { return }
+        
+        do {
+            currentUser = try await DatabaseHelper().getUsers().first
+            let products = try await DatabaseHelper().getProducts()
+            heroProduct = products.first
+            
+            var rows: [ProductRow] = []
+            let allBrands = Set(products.map({ $0.brand }))
+            for brand in allBrands {
+//                let products = self.products.filter({ $0.brand == brand })
+                rows.append(ProductRow(title: brand.capitalized, products: products.shuffled()))
+            }
+            productRows = rows
+        } catch {
+            
+        }
     }
     
     private var header: some View{
@@ -57,6 +104,56 @@ struct NetflixHomeView: View {
                     }
             }
             .font(.title2)
+        }
+    }
+    
+    private func heroCell(product: Product) -> some View {
+        NetflixHeroCell(
+            imageName: product.firstImage,
+            isNetflixFilm: true,
+            title: product.title,
+            categories: [product.category.capitalized, product.brand],
+            onBackgroundPressed: {
+                
+            },
+            onPlayPressed: {
+                
+            },
+            onMyListPressed: {
+                
+            }
+        )
+        .padding(24)
+    }
+    
+    private var categoryRows: some View {
+        LazyVStack(spacing: 16) {
+            ForEach(Array(productRows.enumerated()), id: \.offset) { (rowIndex, row) in
+                VStack(alignment: .leading, spacing: 6) {
+                    Text(row.title)
+                        .font(.headline)
+                        .padding(.horizontal, 16)
+                    
+                    ScrollView(.horizontal) {
+                        LazyHStack {
+                            ForEach(Array(row.products.enumerated()), id: \.offset) { (index, product) in
+                                NetflixMovieComponent(
+                                    imageName: product.firstImage,
+                                    title: product.title,
+                                    isRecentlyAdded: product.recentlyAdded,
+                                    topTenRanking: rowIndex == 1 ? (index + 1) : nil
+                                )
+                                .onTapGesture {
+//                                    onProductPressed(product: product)
+                                }
+                            }
+                        }
+                        .padding(.horizontal, 16)
+
+                    }
+                    .scrollIndicators(.hidden)
+                }
+            }
         }
     }
 }
