0eb99ff88d70c824e42047f7e5457b11ce12f482
Priyammm05 <priyam05soni@gmail.com>
2024-05-21 15:03:38 +0530

    Created the share button using the ShareLink

---
+++ swift-ui-clone/Netflix/Component/ShareButton.swift
@@ -0,0 +1,39 @@
+//
+//  ShareButton.swift
+//  swift-ui-clone
+//
+//  Created by Priyam Soni on 21/05/24.
+//
+
+import SwiftUI
+
+struct ShareButton: View {
+    
+    var url: String = "https://github.com/Priyammm05"
+    
+    @ViewBuilder
+    var body: some View {
+        if let url = URL(string: url) {
+            ShareLink(item: url) {
+                VStack(spacing: 8) {
+                    Image(systemName: "paperplane")
+                        .font(.title)
+                    
+                    Text("Share")
+                        .font(.caption)
+                        .foregroundStyle(.netflixLightGray)
+                }
+                .foregroundStyle(.netflixWhite)
+                .padding(8)
+                .background(Color.black.opacity(0.001))
+            }
+        }
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.black.ignoresSafeArea()
+        ShareButton()
+    }
+}
