65575eb7e1cc6eac19296520640ef117dccb9128
Priyammm05 <priyam05soni@gmail.com>
2024-05-21 15:04:15 +0530

    Added the components in the Detail view

--- swift-ui-clone/Netflix/Core/NetflixDetailView.swift
+++ swift-ui-clone/Netflix/Core/NetflixDetailView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct NetflixDetailView: View {
+    
+    @Environment(\.router) var router
+
     var product: Product = .mock
+    
     @State private var progress: Double = 0.2
+    @State private var isMyList: Bool = false
+    @State private var products: [Product] = []
     
     var body: some View {
         ZStack{
@@ -24,18 +30,40 @@ struct NetflixDetailView: View {
                         
                     },
                     onXMarkPressed: {
-                        
+                        router.dismissScreen()
                     }
                 )
                 
                 ScrollView(.vertical){
                     VStack(alignment: .leading, spacing: 16){
                         detailsProductSection
+                        buttonsSection
+                        productsGridSection
                     }
                     .padding(8)
                 }
                 .scrollIndicators(.hidden)
             }
+        }
+        .task {
+            await getData()
+        }
+        .toolbar(.hidden, for: .navigationBar)
+    }
+    
+    private func onProductPressed(product: Product) {
+          router.showScreen(.sheet) { _ in
+              NetflixDetailView(product: product)
+          }
+      }
+    
+    private func getData() async {
+        guard products.isEmpty else { return }
+        
+        do {
+            products = try await DatabaseHelper().getProducts()
+        } catch {
+            
         }
     }
     
@@ -56,8 +84,52 @@ struct NetflixDetailView: View {
             }
         )
     }
+    
+    private var buttonsSection: some View {
+        HStack(spacing: 32) {
+            MyListButton(isMyList: isMyList) {
+                isMyList.toggle()
+            }
+            
+            RateButton { selection in
+                // do something with selection
+            }
+            
+            ShareButton()
+        }
+        .padding(.leading, 32)
+    }
+    
+    private var productsGridSection: some View {
+        VStack(alignment: .leading) {
+            Text("More Like This")
+                .font(.headline)
+            
+            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 8), count: 3),
+                      alignment: .center,
+                      spacing: 8,
+                      pinnedViews: [],
+                      content: {
+                ForEach(products) { product in
+                    NetflixMovieComponent(
+                        imageName: product.firstImage,
+                        title: product.title,
+                        isRecentlyAdded: product.recentlyAdded,
+                        topTenRanking: nil
+                    )
+                    .onTapGesture {
+                        onProductPressed(product: product)
+                    }
+                }
+            })
+        }
+        .foregroundStyle(.netflixWhite)
+    }
 }
 
+import SwiftfulRouting
 #Preview {
-    NetflixDetailView()
+    RouterView { _ in
+        NetflixDetailView()
+    }
 }
